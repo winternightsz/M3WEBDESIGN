c06df8e1c1656a0e785b890362d734c1fa830063
Izabela <104799350+winternightsz@users.noreply.github.com>
2023-06-27 06:19:30 +0000

    favicon updates

--- js/main.js
+++ js/main.js
@@ -71,3 +71,42 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   themeSwitcherItems.forEach((item) => {
     item.addEventListener("click", onThemeSwitcherItemClick);
   });
+  //////////////////////////////////////////////////////////////////////////////////
+
+  function enviarEmail() {
+    const emailInput = document.getElementById('emailInput');
+    const userEmail = emailInput.value;
+  
+    // Verifique se o campo de e-mail não está vazio
+    if (userEmail !== '') {
+      // Envie uma solicitação POST para o Formspree para enviar o e-mail
+      fetch('https://formspree.io/shawn5harmonizer@gmail.com', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: userEmail }),
+      })
+        .then(response => {
+          // Verifique a resposta do Formspree
+          if (response.ok) {
+            // Email enviado com sucesso
+            alert('Obrigado por se inscrever! Um e-mail foi enviado para você.');
+            // Limpe o campo de entrada de e-mail
+            emailInput.value = '';
+          } else {
+            // Ocorreu um erro ao enviar o e-mail
+            alert('Ocorreu um erro ao enviar o e-mail. Por favor, tente novamente mais tarde.');
+          }
+        })
+        .catch(error => {
+          console.error('Erro:', error);
+          // Ocorreu um erro ao enviar a solicitação
+          alert('Ocorreu um erro ao enviar a solicitação. Por favor, tente novamente mais tarde.');
+        });
+    } else {
+      // Caso o campo de e-mail esteja vazio
+      alert('Por favor, insira seu endereço de e-mail.');
+    }
+  }
+  
